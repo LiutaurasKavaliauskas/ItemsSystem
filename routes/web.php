8c40d48da73b283f30786923bd5e1d2aaa42d893
Liutauras Kavaliauskas <liutauraskav@gmail.com>
2017-05-09 19:46:52 +0300

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,15 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::group(['prefix' => 'items'], function (){
     Route::get('/', ['as' => 'Items', 'uses' => 'ItemsController@index']);
     Route::post('create', ['as' => 'items.create', 'uses' => 'ItemsController@create']);
     Route::post('edit/{id}', ['as' => 'items.edit', 'uses' => 'ItemsController@edit']);
     Route::get('delete/{id}', ['as' => 'items.delete', 'uses' => 'ItemsController@destroy']);
+});
+
+Route::group(['prefix' => 'categories'], function (){
+    Route::get('/', ['as' => 'Categories', 'uses' => 'CategoriesController@index']);
+    Route::post('create', ['as' => 'categories.create', 'uses' => 'CategoriesController@create']);
+    Route::post('edit/{id}', ['as' => 'categories.edit', 'uses' => 'CategoriesController@edit']);
+    Route::get('delete/{id}', ['as' => 'categories.delete', 'uses' => 'CategoriesController@destroy']);
 });
