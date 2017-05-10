c6bcb2c3990f8f8c1f04b38bbff0f01ecb9b54d3
Liutauras Kavaliauskas <liutauraskav@gmail.com>
2017-05-10 13:43:19 +0300

    Added translations

---
+++ resources/lang/en/categories.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'page'    =>
+        [
+            'title' => 'Categories',
+        ],
+    'table'   =>
+        [
+            'title' => 'Title',
+        ],
+    'buttons' =>
+        [
+            'new'    => 'New category',
+            'edit'   => 'Edit category',
+            'delete' => 'Delete category',
+            'items'  => 'Show items in category',
+            'save'   => 'Save',
+            'close'  => 'Close',
+        ],
+    'forms'   =>
+        [
+            'edit' => 'Category Edit Form',
+            'new'  => 'New Category Form',
+        ]
+];
---
+++ resources/lang/en/items.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'page'    =>
+        [
+            'title' => 'Items',
+        ],
+    'table'   =>
+        [
+            'title'       => 'Title',
+            'count'       => 'Count',
+            'description' => 'Description',
+            'category'    => 'Category',
+        ],
+    'buttons' =>
+        [
+            'new'    => 'New item',
+            'edit'   => 'Edit item',
+            'delete' => 'Delete item',
+            'save'   => 'Save',
+            'close'  => 'Close',
+        ],
+    'forms'   =>
+        [
+            'edit' => 'Item Edit Form',
+            'new'  => 'New Item Form',
+        ]
+];
