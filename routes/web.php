0ed3e10623ae629baba46072694291a48f86606b
Liutauras Kavaliauskas <liutauraskav@gmail.com>
2017-05-10 15:49:33 +0300

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
+Route::get('/', ['as' => 'index', 'uses' => 'IndexController@index']);
+
 Route::group(['prefix' => 'api'], function (){
    Route::get('items/{id}', ['as' => 'api.items', 'uses' => 'ItemsController@showItem']);
 });
 
 Route::group(['prefix' => 'items'], function (){
-    Route::get('/', ['as' => 'Items', 'uses' => 'ItemsController@index']);
+    Route::get('/', ['as' => 'items', 'uses' => 'ItemsController@index']);
     Route::post('create', ['as' => 'items.create', 'uses' => 'ItemsController@create']);
     Route::post('edit/{id}', ['as' => 'items.edit', 'uses' => 'ItemsController@edit']);
     Route::get('delete/{id}', ['as' => 'items.delete', 'uses' => 'ItemsController@destroy']);
 });
 
 Route::group(['prefix' => 'categories'], function (){
-    Route::get('/', ['as' => 'Categories', 'uses' => 'CategoriesController@index']);
+    Route::get('/', ['as' => 'categories', 'uses' => 'CategoriesController@index']);
     Route::get('/{id}', ['as' => 'categories.show', 'uses' => 'CategoriesController@showItems']);
     Route::post('create', ['as' => 'categories.create', 'uses' => 'CategoriesController@create']);
     Route::post('edit/{id}', ['as' => 'categories.edit', 'uses' => 'CategoriesController@edit']);
